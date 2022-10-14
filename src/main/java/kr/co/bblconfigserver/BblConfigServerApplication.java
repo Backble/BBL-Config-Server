2b14b50d304501270d6ecb1f96ad11b072c04ba2
홍혁준 <jjj8337721@naver.com>
2022-10-14 13:43:03 +0900

    init

---
+++ src/main/java/kr/co/bblconfigserver/BblConfigServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bblconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BblConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BblConfigServerApplication.class, args);
+    }
+
+}
